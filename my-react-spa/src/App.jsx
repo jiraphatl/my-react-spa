a383d492496aa08dcbdb4350d35d82373bea8ad0
jiraphatl <jiraphat.luef@dome.tu.ac.th>
2025-02-04 12:35:42 +0700

    Lec-3

--- my-react-spa/src/App.jsx
+++ my-react-spa/src/App.jsx
@@ -1,25 +1,91 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import './App.css'
+import Header from './Header';
+import Button from './Button';
+import { configureStore, createSlice } from '@reduxjs/toolkit';
+import { Provider, useDispatch, useSelector } from 'react-redux';
 
+function Home() {
+  return (
+    <div>
+      <Header title="React Workshop" />
+      <p>เรียนรู้พื้นฐานของ React ผ่านการปฏิบัติจริง</p>
+    </div>
+  );
+}
+
+function About() {
+  const handleClick = () => {
+    alert('ปุ่มถูกคลิ้กแล้ว!');
+  };
+  return (
+    <div>
+      <Header title="React Workshop" />
+      <p>เรียนรู้พื้นฐานของ React ผ่านการปฏิบัติจริง</p>
+      <Button label="คลิกตรงนี้" onClick={handleClick} />
+    </div>
+  );
+}
+
+function Greeting() {
+  const user = {
+    name: 'John Doe',
+    age: 25,
+  };
+
+  return (
+    <div>
+      <Header title="React Workshop" />
+      <p>ชื่อ: {user.name}, อายุ: {user.age}</p>
+      <Button label="ตกลง" onClick={() => alert('สวัสดี ' + user.name)} />
+    </div>
+  );
+}
+
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState: { value: 0 },
+  reducers: {
+    increment: (state) => { state.value += 1; },
+    decrement: (state) => { state.value -= 1; }
+  }
+});
+
+const store = configureStore({ reducer: { counter: counterSlice.reducer } });
+
+function Counter() {
+  const count = useSelector(state => state.counter.value);
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => dispatch(counterSlice.actions.increment())}>+</button>
+      <button onClick={() => dispatch(counterSlice.actions.decrement())}>-</button>
+    </div>
+  );
+}
+  
+function Store(){
+  return (
+    <Provider store={store}>
+      <Counter />
+    </Provider>
+  );
+}
 function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li><Link to="/">หน้าแรก</Link></li>
-            <li><Link to="/about">เกี่ยวกับเรา</Link></li>
-          </ul>
-        </nav>
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </div>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/greeting">Greeting</Link> | <Link to="/store">Store</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/greeting" element={<Greeting />} />
+        <Route path="/store" element={<Store />} />
+      </Routes>
     </Router>
-  )
+  );
 }
-
-export default App
+export default App;
